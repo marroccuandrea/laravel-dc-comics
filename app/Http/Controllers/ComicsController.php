e9a9e4920389f2775c26c61716ace81f4bb5b072
marroccuandrea <marroccuandrea@yahoo.it>
2024-05-20 15:56:56 +0200

    [add]Validation form create

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -30,6 +30,42 @@ class ComicsController extends Controller
      */
     public function store(Request $request)
     {
+
+
+        $request->validate([
+            'title' => 'required|min:5|max:255',
+            'description' => 'max:255',
+            'thumb' => 'required|min:5|max:255',
+            'price' => 'required',
+            'series' => 'required|min:5|max:255',
+            'sale_date' => 'required',
+            'type' => 'required|min:5|max:255',
+            'artists' => 'required|min:5|max:255',
+            'writers' => 'required|min:5|max:255',
+        ], [
+            'title.required' => 'Il titolo è un campo obbligatorio',
+            'title.min' => 'Il titolo deve avere almeno :min caratteri',
+            'title.max' => 'Il titolo deve avere al massimo :max caratteri',
+            'description.max' => 'La descrizione deve avere al massimo :max caratteri',
+            'thumb.required' => 'La thumb è un campo obbligatorio',
+            'thumb.min' => 'La thumb deve avere almeno :min caratteri',
+            'thumb.max' => 'La thumb deve avere al massimo :max caratteri',
+            'price.required' => 'Il prezzo è un campo obbligatorio',
+            'series.required' => 'La serie è un campo obbligatorio',
+            'series.min' => 'La serie deve avere almeno :min caratteri',
+            'series.max' => 'La serie deve avere al massimo :max caratteri',
+            'sale_date.required' => 'La data di uscita è un campo obbligatorio',
+            'type.required' => 'Il tipo è un campo obbligatorio',
+            'type.min' => 'Il tipo deve avere almeno :min caratteri',
+            'type.max' => 'Il tipo deve avere al massimo :max caratteri',
+            'artists.required' => 'Gli artisti sono un campo obbligatorio',
+            'artists.min' => 'Gli artisti devono avere almeno :min caratteri',
+            'artists.max' => 'Gli artisti devono avere al massimo :max caratteri',
+            'writers.required' => 'I writer sono un campo obbligatorio',
+            'writers.min' => 'I writer devono avere almeno :min caratteri',
+            'writers.max' => 'I writer devono avere al massimo :max caratteri',
+        ]);
+
         $form_data = $request->all();
 
         $new_comic = new Comic();
@@ -70,6 +106,8 @@ class ComicsController extends Controller
      */
     public function update(Request $request, Comic $comic)
     {
+
+
         $form_data = $request->all();
 
         if ($form_data['title'] === $comic->title) {
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,44 +2,89 @@
 
 @section('content')
     <h1 class="titolo">Crea Fumetto</h1>
+    @if ($errors->any())
+        <div class="alert alert-danger" role="alert">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form action="{{ route('comics.store') }}" method="POST">
         @csrf
         <div class="mb-3">
-            <label for="title" class="form-label">Titolo</label>
-            <input name="title" type="text" class="form-control" id="title">
+            <label for="title" class="form-label fw-bold fs-5">Titolo</label>
+            <input name="title" type="text" class="form-control @error('title') is-invalid @enderror" id="title"
+                value="{{ old('title') }}">
+            @error('title')
+                <small class="text-danger fw-bolder fs-6 ">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
-            <label for="description" class="form-label">Descrizione</label>
-            <textarea name="description" class="form-control" id="description" rows="3"></textarea>
+            <label for="description" class="form-label fw-bold fs-5">Descrizione</label>
+            <textarea name="description" class="form-control @error('description') is-invalid @enderror" id="description"
+                rows="3">{{ old('description') }}</textarea>
+            @error('description')
+                <small class="text-danger fw-bolder fs-6 ">{{ $message }}</small>
+            @enderror
         </div>
 
         <div class="mb-3">
-            <label for="thumb" class="form-label">URL Immagine</label>
-            <input name="thumb" type="text" class="form-control" id="thumb">
+            <label for="thumb" class="form-label fw-bold fs-5">URL Immagine</label>
+            <input name="thumb" type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb"
+                value="{{ old('thumb') }}">
+            @error('thumb')
+                <small class="text-danger fw-bolder fs-6 ">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
-            <label for="price" class="form-label">Prezzo</label>
-            <input name="price" type="text" class="form-control" id="price">
+            <label for="price" class="form-label fw-bold fs-5">Prezzo</label>
+            <input name="price" type="text" class="form-control @error('price') is-invalid @enderror" id="price"
+                value="{{ old('price') }}">
+            @error('price')
+                <small class="text-danger fw-bolder fs-6 ">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
-            <label for="series" class="form-label">Serie</label>
-            <input name="series" type="text" class="form-control" id="series">
+            <label for="series" class="form-label fw-bold fs-5">Serie</label>
+            <input name="series" type="text" class="form-control @error('series') is-invalid @enderror" id="series"
+                value="{{ old('series') }}">
+            @error('series')
+                <small class="text-danger fw-bolder fs-6 ">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
-            <label for="sale_date" class="form-label">Data uscita</label>
-            <input name="sale_date" type="date" class="form-control" id="sale_date">
+            <label for="sale_date" class="form-label fw-bold fs-5">Data uscita</label>
+            <input name="sale_date" type="date" class="form-control @error('sale_date') is-invalid @enderror"
+                id="sale_date" value="{{ old('sale_date') }}">
+            @error('sale_date')
+                <small class="text-danger fw-bolder fs-6 ">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
-            <label for="type" class="form-label">Tipo</label>
-            <input name="type" type="text" class="form-control" id="type">
+            <label for="type" class="form-label fw-bold fs-5">Tipo</label>
+            <input name="type" type="text" class="form-control @error('type') is-invalid @enderror" id="type"
+                value="{{ old('type') }}">
+            @error('type')
+                <small class="text-danger fw-bolder fs-6 ">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
-            <label for="artists" class="form-label">Disegantore</label>
-            <input name="artists" type="text" class="form-control" id="artists">
+            <label for="artists" class="form-label fw-bold fs-5">Disegantore</label>
+            <input name="artists" type="text" class="form-control @error('artists') is-invalid @enderror" id="artists"
+                value="{{ old('artists') }}">
+            @error('artists')
+                <small class="text-danger fw-bolder fs-6 ">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
-            <label for="writers" class="form-label">Scrittore</label>
-            <input name="writers" type="text" class="form-control" id="writers">
+            <label for="writers" class="form-label fw-bold fs-5">Scrittore</label>
+            <input name="writers" type="text" class="form-control @error('writers') is-invalid @enderror" id="writers"
+                value="{{ old('writers') }}">
+            @error('writers')
+                <small class="text-danger fw-bolder fs-6 ">{{ $message }}</small>
+            @enderror
         </div>
 
         <button class="btn btn-success" type="submit">Genera Fumetto</button>
